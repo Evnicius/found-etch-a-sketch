3c30b124f170cd0ad71cf9c1a3e273926b166b44
Evnicius <justevnic@gmail.com>
2022-05-31 19:51:34 -0700

    Added Reset and RGB buttons and their functionalities to etch-a-sketch.js

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,9 @@
 const body = document.querySelector("body");
 const grid = document.createElement("div");
+const divButton = document.createElement("div");
 const buttonPromptGrid = document.createElement("button");
+const buttonReset = document.createElement("button");
+const buttonRGB = document.createElement("button");
 
 grid.id = "grid";
 grid.style.cssText ="display:flex;flex-direction:column;align-items:center; width:fit-content;border: solid 0.1px grey";
@@ -39,37 +42,59 @@ function randomInteger()
     return Math.floor(Math.random()*255);
 }
 
-body.appendChild(buttonPromptGrid);
-insertDivs(10);
+divButton.appendChild(buttonPromptGrid);
+divButton.appendChild(buttonReset);
+divButton.appendChild(buttonRGB);
+body.append(divButton);
+
+let sizeGrid = 10;
+insertDivs(sizeGrid);
 
 buttonPromptGrid.textContent = "Set Size";
 buttonPromptGrid.style.cssText = "background-color: #121212; color: white; border: 1px solid #f44336; border-radius: 12px; padding: 5px 20px; margin: 20px; font-size: 20px";
 document.body.style.backgroundColor = "#121212";
 
-function paintDiv () {
+buttonReset.textContent = "Reset";
+buttonReset.style.cssText = "background-color: #121212; color: white; border: 1px solid #f44336; border-radius: 12px; padding: 5px 20px; margin: 20px; font-size: 20px";
+buttonRGB.textContent = "RGB";
+buttonRGB.style.cssText = "background-color: #121212; color: white; border: 1px solid #f44336; border-radius: 12px; padding: 5px 20px; margin: 20px; font-size: 20px";
+buttonRGB.value = 0;
+
+let toggleRGBBool = buttonRGB.value;
+
+function paintDiv (valueRGB) {
     let listenDivOuter  = document.querySelectorAll("#tinyDiv");
     listenDivOuter.forEach((div) => {
         div.addEventListener('mouseover', (div) => {
             let colorRed = randomInteger();
             let colorGreen = randomInteger();
             let colorBlue = randomInteger();
-            let randomColor = "rgb(" + colorRed + "," + colorGreen + "," + colorBlue + ")";
+            let setColor = "white";
+
+            if (valueRGB == 1)
+            {
+                setColor = "rgb(" + colorRed + "," + colorGreen + "," + colorBlue + ")";
+            }
             
             div.stopPropagation();
-            div.target.style.transition ="0.01s";
-            div.target.style.backgroundColor = randomColor;
+            div.target.style.transition ="0.7s";
+            div.target.style.backgroundColor = setColor;
+            
         });
     
         div.addEventListener("mouseout", (div) => {
             div.stopPropagation();
             div.target.style.transition ="10s";
-            div.target.style.backgroundColor = "";
+            if (valueRGB == 0)
+            {
+                div.target.style.color = "white";
+            }
             
         });
     });
 }
 
-paintDiv();
+paintDiv(toggleRGBBool);
 
 function removeGrid()
 {
@@ -85,23 +110,34 @@ function removeGrid()
 buttonPromptGrid.addEventListener("click", (event) => {
     let inputGridSize = prompt("Enter a value for grid size up to 100 px.\
     \nThe grid is a square.");
+    sizeGrid = inputGridSize;
     if (inputGridSize > 0 && inputGridSize <= 100)
     {
         removeGrid();
         insertDivs(inputGridSize);
-        paintDiv();
+        paintDiv(buttonRGB.value);
     }
     else
     {
         alert("Invalid value!");
     }
-    
-
 });
 
+buttonReset.addEventListener("click", (event) => {
+    removeGrid();
+    insertDivs(sizeGrid);
+    paintDiv(toggleRGBBool);
+})
 
-
-
-
-
-
+buttonRGB.addEventListener("click", (event) => {
+    if (buttonRGB.value == 1)
+    {
+        buttonRGB.value = 0;
+        paintDiv(buttonRGB.value);
+    }
+    else
+    {
+        buttonRGB.value = 1;
+        paintDiv(buttonRGB.value);
+    }
+})
